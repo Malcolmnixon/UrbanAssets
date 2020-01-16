6e95f7901a15ff1cc98c032510358c7e482e62b0
Malcolm Nixon <malcolm.nixon@gmail.com>
2020-01-15 21:57:57 -0500

    Added second building and city block

---
+++ Assets/Editor/UrbanDemoBuild.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public class Builder
+{
+    [MenuItem("Build/UrbanDemo/Build All")]
+    public static void BuildAll() {
+        BuildWin64();
+        BuildLin64();
+        BuildAndroid();
+    }
+
+	[MenuItem("Build/UrbanDemo/Build Windows 64-bit")]
+    public static void BuildWin64()
+    {
+		PrintReport(BuildPipeline.BuildPlayer(
+            new[] {
+				"Assets/Scenes/Urban/UrbanDemo.unity"
+            },
+            "Build/Win64/UrbanDemo.exe",
+            BuildTarget.StandaloneWindows64,
+            BuildOptions.None));
+    }
+
+	[MenuItem("Build/UrbanDemo/Build Linux 64-bit")]
+    public static void BuildLin64()
+    {
+		PrintReport(BuildPipeline.BuildPlayer(
+            new[] {
+				"Assets/Scenes/Urban/UrbanDemo.unity"
+            },
+			"Build/Linux/UrbanDemo.x86_64",
+            BuildTarget.StandaloneLinux64,
+            BuildOptions.None));
+    }
+
+	[MenuItem("Build/UrbanDemo/Build Android")]
+    public static void BuildAndroid()
+    {
+		PrintReport(BuildPipeline.BuildPlayer(
+            new[] {
+				"Assets/Scenes/Urban/UrbanDemo.unity"
+            },
+			"Build/Android/UrbanDemo.apk",
+            BuildTarget.Android,
+            BuildOptions.None));
+    }
+
+
+	private static void PrintReport(BuildReport report) {
+		var summary = report.summary;
+		if (summary.result == BuildResult.Succeeded) {
+			Debug.Log($"Build Succeeded {summary.totalSize} bytes");
+		} else {
+			Debug.Log("Build Failed");
+		}
+	}
+}
---
+++ Assets/Scenes/Urban/DemoMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoMove : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+    public float RotateSpeed = 90f;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -RotateSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, RotateSpeed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            var halfWidth = (Screen.width / 2);
+            var halfHeight = (Screen.height / 2);
+
+            var amountX = (Input.mousePosition.x - halfWidth) / halfWidth;
+            var amountY = (Input.mousePosition.y - halfHeight) / halfHeight;
+
+            var newX = amountX * RotateSpeed * Time.deltaTime;
+            var newZ = amountY * MoveSpeed * Time.deltaTime;
+
+            transform.Rotate(0, newX, 0);
+            transform.Translate(0, 0, newZ);
+        }
+    }
+}
